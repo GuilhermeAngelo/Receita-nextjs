9438149c9e12aa64ea66e47c86057eab3cd7d926
GuilhermeAngelo <guilhermeangelo2001@gmail.com>
2022-06-22 17:42:39 -0300

    next_restante

---
+++ pages/menu.js
@@ -0,0 +1,37 @@
+export function Menu(){
+
+    return (
+ 
+       <h2>
+           Menu principal !!!
+       </h2>
+ 
+    )
+ 
+ }
+ export function Content(){
+    return (
+        <div>
+           <p>Lorem ipsum dolor sit amet consectetur adipiscing elit vitae neque velit sed, lacus ut potenti ornare dapibus convallis placerat sollicitudin aliquam metus, dis condimentum nec diam quam euismod phasellus id auctor efficitur. Ipsum ridiculus tristique interdum platea cras ullamcorper elit, porttitor purus penatibus taciti nibh litora convallis sodales, ac commodo mi faucibus consectetur tellus. Egestas lorem netus scelerisque efficitur ut interdum, torquent nascetur habitasse libero per, magnis id laoreet est a. Habitasse maecenas quis ridiculus nec tellus mi nunc maximus velit mus, aptent ex pretium risus suspendisse cras leo donec tempor, sed facilisis lobortis justo faucibus sodales taciti tristique vitae. Nec ut parturient potenti ipsum fames natoque posuere odio dui, vehicula lorem blandit pretium finibus rhoncus proin orci metus, egestas pharetra lobortis ligula facilisi dictumst fringilla quisque.</p>
+           <p>Maecenas tempus cubilia tristique ridiculus nibh cras porta sem id, fusce blandit euismod et auctor magna ac hac, convallis vulputate quam dictumst lacus praesent egestas ornare. Euismod magnis ultrices ipsum facilisis ornare pulvinar fusce gravida maecenas netus, vivamus tristique dolor litora eros suscipit pretium volutpat. Cras sagittis quisque aliquam lorem bibendum lectus neque vulputate, mattis etiam et parturient ut porta laoreet. Faucibus a ultrices orci varius ullamcorper mus rhoncus curae molestie, turpis parturient hac purus sit leo ut libero fames, consectetur scelerisque pulvinar hendrerit taciti proin pharetra posuere. Sed aptent ad senectus sodales commodo mi iaculis porta facilisi hendrerit, mauris placerat accumsan imperdiet mattis in finibus ut ex pulvinar congue, dui maecenas ligula potenti augue litora erat proin odio. Laoreet nunc semper auctor per inceptos urna tempus mattis natoque libero ex, congue id dictumst netus amet sem justo non odio quam. Lectus augue hac rutrum justo neque luctus tincidunt taciti ultricies, congue sapien nunc nulla dictumst mi ut tempus, inceptos dolor id libero scelerisque laoreet leo cursus.</p>
+           <p>Ante penatibus class elit natoque conubia quam enim, cubilia mauris ipsum metus vehicula volutpat, ac aliquam senectus curae fermentum porta. Posuere quisque habitasse natoque nam magna egestas sodales, facilisi accumsan aptent dapibus sem enim eleifend nulla, quis tempus lacinia justo consequat ullamcorper. Phasellus et fames feugiat maximus ullamcorper placerat neque a, mi hac habitant justo class quisque donec, finibus laoreet morbi habitasse nec odio scelerisque. Purus dis sed ac placerat fusce sodales luctus, per vehicula amet nunc augue in. Eget mollis dapibus duis pretium rhoncus donec nascetur conubia, habitasse pulvinar tempus ultrices a semper quis odio, auctor himenaeos potenti neque dictumst senectus leo. Sodales morbi posuere elit duis platea dictumst, habitasse suscipit egestas tempus ridiculus nec, magnis pulvinar phasellus nostra massa. Magnis suscipit auctor eget pharetra fermentum feugiat felis ad, purus duis fusce est arcu vehicula elit urna, nam nisi nascetur conubia volutpat phasellus maximus.</p>
+           <p>Dictumst potenti habitasse pretium gravida at turpis aptent, pharetra facilisi semper accumsan felis etiam mollis, venenatis dapibus suscipit lobortis vestibulum velit. Eleifend ultricies metus ipsum rutrum suspendisse vitae conubia volutpat torquent aenean tincidunt est, senectus hendrerit platea auctor consectetur curabitur neque bibendum cubilia magna arcu. Ligula libero nisi consequat scelerisque lacinia nascetur, pharetra pretium potenti nec hendrerit, rhoncus facilisi luctus porttitor fames.</p>
+           <p>Taciti vivamus aliquet dui elit maximus nec quam purus facilisi scelerisque est orci lacinia magna, habitant neque cursus venenatis feugiat penatibus justo dolor et imperdiet platea mauris. Posuere blandit dignissim suspendisse tristique sed habitasse urna, consectetur cursus hac dictum porta nisl platea potenti, rutrum sem ornare conubia lacinia tempor. Quam nisi et lectus varius vulputate ante aenean suscipit sed, maximus litora leo bibendum orci facilisi lorem tempus, vivamus ultrices posuere venenatis laoreet hendrerit mattis cubilia. Condimentum nibh nisl dictumst ut quisque mauris fusce habitasse ultrices, ante efficitur aenean sollicitudin justo ad molestie aliquam vivamus, eleifend sodales ex sem magnis curae neque adipiscing. Fermentum fusce ex himenaeos donec convallis magna torquent, sodales aliquet habitasse mi odio penatibus vehicula conubia, class condimentum enim justo lectus bibendum.</p>
+       </div>
+       )
+}
+export default function Principal(){
+
+    return (
+ 
+       <div>
+
+          <Menu/>
+          <hr></hr>
+          <Content/>
+ 
+       </div>
+ 
+    )
+ 
+ }
