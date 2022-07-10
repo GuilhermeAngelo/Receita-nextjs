eef5376d3a4b4686a812caefa08a5c3c95da839c
GuilhermeAngelo <guilhermeangelo2001@gmail.com>
2022-07-10 15:55:46 -0300

    repositórios individuais para cada receita

---
+++ pages/receita_2/index.js
@@ -0,0 +1,22 @@
+
+ import {Links} from "./receita_2/pweb/pweb.js"
+ import {Content} from "./menu"
+ 
+ export function MariaPrea(props){
+   return(
+      <h1>{props.titulo}</h1>
+   )
+ }
+export default function Home(){
+
+   return (
+      <div>
+         <MariaPrea titulo="Morreu Maria prea"/>
+         <Links/>
+         <Content/>
+         
+       </div>
+ 
+    )
+ 
+ }
